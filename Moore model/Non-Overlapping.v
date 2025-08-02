2f481e3b550a0a0486a641da01b03e81a0718eff
Sanjeeth G <sit22ec051@sairamtap.edu.in>
2025-08-02 16:15:26 +0530

    Update Non-Overlapping.v

--- Moore model/Non-Overlapping.v
+++ Moore model/Non-Overlapping.v
@@ -1,52 +1,40 @@
-module sd1001_moore(input bit clk,
-                   input logic reset,
-                   input logic din,
-                   output logic dout);
+module moore_1001_non_overlap (
+  input clk,
+  input reset,
+  input din,
+  output reg dout
+);
 
-  typedef enum logic [2:0] {S0, S1, S2, S3, S4} state_t;
-  state_t state;
+  parameter S0 = 3'b000;
+  parameter S1 = 3'b001;
+  parameter S2 = 3'b010;
+  parameter S3 = 3'b011;
+  parameter S4 = 3'b100;
 
+  reg [2:0] state;
+
+  // State transitions
   always @(posedge clk or posedge reset) begin
-    if(reset) begin
-      dout <= 1'b0;
+    if (reset)
       state <= S0;
-    end
     else begin
-      case(state)
-        S0: begin
-          dout <=1'b0;
-          if(din)
-            state <= S1;
-        end
-        S1: begin
-          dout <= 1'b0;
-          if(~din)
-            state <= S2;
-        end
-        S2: begin
-          dout <= 1'b0;
-          if(~din)
-            state <= S3;
-          else
-            state <= S1;
-        end
-        S3: begin
-          dout <= 1'b0;
-          if(din)
-            state <= S4;
-          else
-            state <= S0;
-        end
-        S4: begin
-          dout <= 1'b1;
-          if(din)
-            state <= S1;
-          else
-            state <= S0;
-        end
+      case (state)
+        S0: state <= (din) ? S1 : S0;
+        S1: state <= (din) ? S1 : S2;
+        S2: state <= (din) ? S1 : S3;
+        S3: state <= (din) ? S4 : S0;
+        S4: state <= S0;  // After detection, reset to initial (non-overlapping)
+        default: state <= S0;
       endcase
     end
   end
 
+  // Output logic
+  always @(*) begin
+    case (state)
+      S4: dout = 1;
+      default: dout = 0;
+    endcase
+  end
 
 endmodule
