eab09c29ecae7ad54fe72b9708a0660ce37bcc51
Sanjeeth G <sit22ec051@sairamtap.edu.in>
2025-08-02 15:04:23 +0530

    Update and rename full_adder_tb.v to Full_Adder/full_adder_tb.v

--- Full_Adder/full_adder_tb.v
+++ Full_Adder/full_adder_tb.v
@@ -1,25 +1,3 @@
-`timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 25.06.2025 10:07:38
-// Design Name: 
-// Module Name: full_adder_tb
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
-
 module full_adder_tb;
 reg a,b,cin;
 wire sum,cout;
