df8730855380ce7a39f8a1dc36f9a5af1b9ba1a7
Sanjeeth G <sit22ec051@sairamtap.edu.in>
2025-08-02 15:07:48 +0530

    Update and rename logic_gates.v to Logic_Gates/logic_gates.v

--- Logic_Gates/logic_gates.v
+++ Logic_Gates/logic_gates.v
@@ -1,25 +1,3 @@
-`timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 24.06.2025 11:44:53
-// Design Name: 
-// Module Name: logic_gates
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
-
 module logic_gates(
     input a,b,
     output and_gate,or_gate,nand_gate,nor_gate,not_gate,xor_gate,xnor_gate
