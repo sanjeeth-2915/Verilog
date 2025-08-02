aeb9e8edb5393161624395d858bd81fc7ca965b9
Sanjeeth G <sit22ec051@sairamtap.edu.in>
2025-08-02 15:06:57 +0530

    Update and rename logic_gate_tb.v to Logic_Gates/logic_gate_tb.v

--- Logic_Gates/logic_gate_tb.v
+++ Logic_Gates/logic_gate_tb.v
@@ -1,25 +1,3 @@
-`timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 24.06.2025 11:51:35
-// Design Name: 
-// Module Name: logic_gate_tb
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
-
 module logic_gate_tb( );
 reg a,b;
 wire and_gate,or_gate,nand_gate,nor_gate,not_gate,xor_gate,xnor_gate;
@@ -41,5 +19,4 @@ initial begin
     #5   a=1; b=1;
     #10 $finish;
 end
-
 endmodule
