3652d2c57d996d8c026b05a97436c7854dde9b3b
Sanjeeth G <sit22ec051@sairamtap.edu.in>
2025-08-18 22:33:04 +0530

    Update 1:2 Demux_tb.v

--- Demux/1:2 Demux_tb.v
+++ Demux/1:2 Demux_tb.v
@@ -3,4 +3,22 @@ reg sel;
   reg din;
   wire y0,y1;
 
-  1:2
+  1:2_demux uut (.sel(sel),
+                 .din(din),
+                 .y0(y0),
+                 .y1(y1)
+                );
+
+  initial begin 
+
+    $monitor(" %b %b | %b %b " , sel,din,y0,y1);
+// Test cases
+    din = 1; sel = 0; #10;  // input -> y0
+    din = 1; sel = 1; #10;  // input -> y1
+    din = 0; sel = 0; #10;  // no output
+    din = 0; sel = 1; #10;  // no output
+
+    #20 $finish;
+  end
+endmodule
+    
