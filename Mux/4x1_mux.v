10efd7d04c78375e4d1aaed8a3723a7ccbc0d84a
Sanjeeth G <sit22ec051@sairamtap.edu.in>
2025-08-18 19:41:54 +0530

    Update 4x1_mux.v

--- Mux/4x1_mux.v
+++ Mux/4x1_mux.v
@@ -1,13 +1,16 @@
-module 4x1_mux ( input wire s0,s1,a,b,c,d, output reg y);
+module mux4x1 (
+  input wire s0, s1,
+  input wire a, b, c, d,
+  output reg y
+);
 
-  always@(*)begin 
+  always @(*) begin 
     case({s1,s0})
       2'b00: y = a;
       2'b01: y = b;
       2'b10: y = c;
       2'b11: y = d;
-      default : y = 1'b0;
+      default: y = 1'b0;
     endcase
-end  
-endmodule 
-  
+  end  
+endmodule
