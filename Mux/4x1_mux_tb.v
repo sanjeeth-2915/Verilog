eb5e479a0ac408968e75c914eaf9e039d51fc4c2
Sanjeeth G <sit22ec051@sairamtap.edu.in>
2025-08-18 19:24:30 +0530

    Update 4x1_mux_tb.v

--- Mux/4x1_mux_tb.v
+++ Mux/4x1_mux_tb.v
@@ -10,7 +10,20 @@ module 4x1_mux_tb;
                .s0(s0),
                .s1(s1)
               );
+  
+  initial begin 
 
+  $display( "%b %b %b %b | %b %b ", a,b,c,d,s0,s1);
+
+  a=1; b=0; c=0; d=0; s0=0; s1=0; #10;
+  a=0; b=1; c=0; d=0; s0=0; s1=0; #10;
+  a=0; b=0; c=1; d=0; s0=0; s1=0; #10;
+  a=0; b=0; c=0; d=1; s0=0; s1=0; #10;
+
+  $finish 
+
+  end
+endmodule 
   
 
   
