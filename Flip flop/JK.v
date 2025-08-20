ba7143f494e3838b28b77746352aa3de8f7bc789
Sanjeeth G <sit22ec051@sairamtap.edu.in>
2025-08-20 22:31:33 +0530

    Create JK.v

---
+++ Flip flop/JK.v
@@ -0,0 +1,13 @@
+module jk_ff (
+    input J, K, clk,
+    output reg Q
+);
+    always @(posedge clk) begin
+      case ({J, K})
+            2'b00: Q <= Q;       // No change
+            2'b01: Q <= 0;       // Reset (0)
+            2'b10: Q <= 1;       // Set(1)
+            2'b11: Q <= ~Q;    // Toggle the state 
+        endcase
+    end
+endmodule
