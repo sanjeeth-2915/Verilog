e54677c9a935bd268c58816beaef9834425670a1
Sanjeeth G <sit22ec051@sairamtap.edu.in>
2025-08-21 11:44:56 +0530

    Update SR_tb.v

--- Flip flop/SR_tb.v
+++ Flip flop/SR_tb.v
@@ -1,2 +1,22 @@
 module sr_ff_tb;
+  reg S,R,clk;
+  wire Q;
+
+  sr_ff uut (.S(S),
+             .R(R),
+             .clk(clk),
+             .Q(Q)
+            );
+  initial begin 
+    $monitor("Time = %0t | S=%b,R=%b | Q=%b", $time, S,R,Q);
+    
+
+
+
+
+
+    $finish;
+  end
+endmodule 
+
   
