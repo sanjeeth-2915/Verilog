c52cd2fd03ff1c8e340f4f261c8368381c0ee565
Sanjeeth G <sit22ec051@sairamtap.edu.in>
2025-09-02 22:26:54 +0530

    Update magnitude_comparator.v

--- Magnitude_comparator/magnitude_comparator.v
+++ Magnitude_comparator/magnitude_comparator.v
@@ -1,17 +1,24 @@
 module magnitude_comparator_16 (
-    input logic [15:0] A, B,
-    output logic A_gt_B, A_eq_B, A_lt_B
+    input  [15:0]A, [15:0]B,
+    output A_gt_B, A_eq_B, A_lt_B
 );
-    always_comb begin
-        A_gt_B = 0;
-        A_eq_B = 0;
-        A_lt_B = 0;
-
-        if (A > B)
+    always@(*)begin
+        if (A>B) begin
             A_gt_B = 1;
-        else if (A < B)
-            A_lt_B = 1;
-        else
+            A_eq_B = 0;
+            A_lt_B = 0;
+        end
+
+        else if (A==B) begin
+            A_gt_B = 0;
             A_eq_B = 1;
-    end
+            A_lt_B = 0;
+         end
+        
+         else (A<B) begin
+            A_gt_B = 0;
+            A_eq_B = 0;
+            A_lt_B = 1;
+          end
+    end 
 endmodule
