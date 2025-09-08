c78a1e1bd144cfa0e2f9b56cb2d29bac6137f2f8
Sanjeeth G <sit22ec051@sairamtap.edu.in>
2025-09-08 21:58:30 +0530

    Update 4bit up_down counter_tb.v

--- Counter/4bit up_down counter_tb.v
+++ Counter/4bit up_down counter_tb.v
@@ -1,1 +1,32 @@
+module up_down_counter_tb;
+    reg clk;
+    reg reset;
+    reg up_down;
+    wire [3:0] count;
 
+  
+    up_down_counter uut (
+        .clk(clk),
+        .reset(reset),
+        .up_down(up_down),
+        .count(count)
+    );
+
+    initial 
+      clk = 0;
+    always #5 
+      clk = ~clk;
+
+    initial begin
+        reset = 1;
+        up_down = 1; 
+        #10 reset = 0;
+        #100 up_down = 0; 
+        #100;
+        $finish;
+    end
+
+    initial begin
+        $monitor("Time=%0d, clk=%b, reset=%b, up_down=%b, count=%b", $time, clk, reset, up_down, count);
+    end
+endmodule
