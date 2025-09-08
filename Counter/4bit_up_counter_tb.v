d41f2a5d0f503487b05962826f9be9029e0fdbce
Sanjeeth G <sit22ec051@sairamtap.edu.in>
2025-09-08 22:06:09 +0530

    Create 4bit_up_counter_tb.v

---
+++ Counter/4bit_up_counter_tb.v
@@ -0,0 +1,25 @@
+module up_counter_tb;
+    reg clk;
+    reg reset;
+    wire [3:0] count;
+
+   
+    up_counter uut (
+        .clk(clk),
+        .reset(reset),
+        .count(count)
+    );
+
+    initial clk = 0;
+    always #5 clk = ~clk;
+
+    initial begin
+        reset = 1;
+        #10 reset = 0;
+        #100;
+        $finish;
+    end
+    initial begin
+        $monitor("Time=%0d, clk=%b, reset=%b, count=%b", $time, clk, reset, count);
+    end
+endmodule
