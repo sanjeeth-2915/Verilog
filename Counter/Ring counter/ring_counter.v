cded8aa6744916109a8a23ce0f38c6f10265d24f
Sanjeeth G <sit22ec051@sairamtap.edu.in>
2025-09-13 12:03:39 +0530

    Update ring_counter.v

--- Counter/Ring counter/ring_counter.v
+++ Counter/Ring counter/ring_counter.v
@@ -1,1 +1,13 @@
+module ring_counter(
+    input clk,
+    input reset,
+    output reg [3:0] q
+);
+    always @(posedge clk or posedge reset) begin
+        if (reset)
+            q <= 4'b0001; 
+        else
+            q <= {q[2:0], q[3]};
+    end
+endmodule
 
